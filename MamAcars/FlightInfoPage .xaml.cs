2c73a6c9216d8ec3bf26cdb5f6bb416f1be8a483
Unai Sarasola <usarasola@stratio.com>
2024-12-31 13:47:22 +0100

    WIP

---
+++ MamAcars/FlightInfoPage .xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MamAcars
+{
+    public partial class FlightInfoPage : Page
+    {
+        private Action _onStartFlight;
+
+        public FlightInfoPage(Action onStartFlight)
+        {
+            InitializeComponent();
+            _onStartFlight = onStartFlight;
+        }
+
+        private void OnStartFlight(object sender, RoutedEventArgs e)
+        {
+            _onStartFlight();
+        }
+    }
+}
---
+++ MamAcars/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MamAcars
+{
+    public partial class LoginPage : Page
+    {
+        private Action _onLoginSuccess;
+
+        public LoginPage(Action onLoginSuccess)
+        {
+            InitializeComponent();
+            _onLoginSuccess = onLoginSuccess;
+        }
+
+        private void OnLoginClicked(object sender, RoutedEventArgs e)
+        {
+            // Aquí haces la autenticación, si es exitosa:
+            _onLoginSuccess();
+        }
+    }
+}
