9e4149524fb1aa2859ce30d945efd2d4319d10ce
Unai Sarasola <unaisarasolaalvarez@gmail.com>
2025-12-31 16:41:27 +0100

    Added confirmation exit

---
+++ MamAcars/Contracts/IPageWithUnsavedChanges.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MamAcars.Contracts
+{
+    public interface IPageWithUnsavedChanges
+    {
+        bool HasUnsavedChanges { get; }
+    }
+
+}
